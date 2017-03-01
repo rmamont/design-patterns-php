2d2e838db4c9460c8c7b78cc2912309c136e3487
rmamont <webmaster@redmamont.ru>
2017-03-01 13:49:53 +1000

    Add web-page for Behavioral patterns

---
+++ public/behavioral.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: nnrepin
+ * Date: 01.03.2017
+ * Time: 13:07
+ */
+
+use \DesignPatterns\Behavioral;
+
+/**
+ * Behavioral\Chain Of Responsibility
+ */
+echo '>> Behavioral\Chain Of Responsibility', '<br><br>';
+
+
+echo '<hr>';
